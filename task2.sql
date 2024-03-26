28f969d6f7ab4d97483f75df8286e98591676b5e
AnalyticalJewel <jewel12345alam@gmail.com>
2024-03-26 17:17:36 +0530

    task executed at one short

---
+++ task2.sql
@@ -0,0 +1,4 @@
+/* Write a query that, upon execution, allows you to check the result set contained in 
+the male_max_salaries temporary table you created in the previous exercise.*/
+
+select * from maximum_male_salary;
