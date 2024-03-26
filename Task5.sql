fc947319d9bc0e793844138a1be80daaa95476bf
JEWEL ALAM <163345491+AnalyticalJewel@users.noreply.github.com>
2024-03-26 18:23:42 +0530

    Update of commit message
    It is the query to check the result of the task 4

--- Task5.sql
+++ Task5.sql
@@ -1,4 +1,4 @@
 /* Write a query that, upon execution, allows you to check the result set contained in the dates 
-temporary table you created in the previous exercise. */
+temporary table you created in the previous exercise.. */
 
 Select * from dates ;
