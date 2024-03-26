42357fcd0265b8d7617ad604179fc96baf61a2f5
AnalyticalJewel <jewel12345alam@gmail.com>
2024-03-26 18:14:11 +0530

    this task is associated with the formation of new table and the interpretation of real time date. the output of the task will depend on the time of task execution

---
+++ Task4.sql
@@ -0,0 +1,16 @@
+/* Create a temporary table called dates containing the following three columns:
+
+- one displaying the current date and time,
+
+- another one displaying 2 months earlier than the current date and time, and a
+
+- third column displaying 2 years later than the current date and time.
+Task by: Jewel Alam
+*/ 
+
+CREATE TEMPORARY TABLE dates AS
+SELECT 
+    NOW() AS curr_dateandtime,
+    DATE_SUB(NOW(), INTERVAL 2 MONTH) AS a_month_earlier,
+    DATE_ADD(NOW(), INTERVAL 2 YEAR) AS a_year_later;
+    select * from temp_date;
